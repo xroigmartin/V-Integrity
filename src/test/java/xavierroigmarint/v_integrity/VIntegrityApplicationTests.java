c5e03e226bf84cd1b05a59083076bd217c3ee598
Xavier Roig <xroigmartin@gmail.com>
2025-12-21 08:14:07 +0100

    feat: init Spring Boot project with Actuator
    - Initialize project using Spring Initializr.
    - Add dependencies: WebMVC, Validation, and Actuator.
    - Verify application startup and health check endpoint.

---
+++ src/test/java/xavierroigmarint/v_integrity/VIntegrityApplicationTests.java
@@ -0,0 +1,13 @@
+package xavierroigmarint.v_integrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VIntegrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
